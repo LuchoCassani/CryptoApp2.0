7bf8456db12e7ce79fb45172da527c8b4a7d6dec
Luciano Cassani <lucho_92@hotmail.com>
2022-07-28 23:02:25 -0300

    crypto detail layout

--- app/src/main/java/com/cassani/cryptoapp20/view/MainActivity.kt
+++ app/src/main/java/com/cassani/cryptoapp20/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.cassani.cryptoapp20
+package com.cassani.cryptoapp20.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cassani.cryptoapp20.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
